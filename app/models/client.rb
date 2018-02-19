23e12b9c6a43adee0a78fabec0ee38b6e46e28dc
jean <jeanbiras@free.fr>
2018-02-19 06:27:30 -0800

    Support du nested form dans le modèle du client

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,10 +3,13 @@ class Client < ApplicationRecord
   has_many :etudes
   has_many :clients_enfants
   has_many :enfants, :through => :clients_enfants
-  has_many :clients_conjoints
+  accept_nested_attributes_for :clients_enfants
+  has_one :clients_conjoint
   has_many :clients_employeurs
   has_many :employeurs, :through => :clients_employeurs
+  accept_nested_attributes_for :clients_employeurs
   has_many :clients_etats_civils
-  has_many :etats_civils, :through => :clients_etats_civil
+  has_many :etats_civils, :through => :clients_etats_civils
+  accept_nested_attributes_for :clients_etats_civils
 
 end
