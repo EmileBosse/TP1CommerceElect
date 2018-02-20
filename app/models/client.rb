728d6d41d1e9e23983e2efe2afeeee8bd092f2bc
jean <jeanbiras@free.fr>
2018-02-19 17:55:19 -0800

    Correction d'un bug dans l'intégration du nested form dans le modèle de client + utilisation de bootstrap dans l'affichage des tableaux

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,15 +1,16 @@
 class Client < ApplicationRecord
   belongs_to :adress
   has_many :etudes
+  accepts_nested_attributes_for :etudes
   has_many :clients_enfants
   has_many :enfants, :through => :clients_enfants
-  accept_nested_attributes_for :clients_enfants
+  accepts_nested_attributes_for :clients_enfants
   has_one :clients_conjoint
   has_many :clients_employeurs
   has_many :employeurs, :through => :clients_employeurs
-  accept_nested_attributes_for :clients_employeurs
+  accepts_nested_attributes_for :clients_employeurs
   has_many :clients_etats_civils
   has_many :etats_civils, :through => :clients_etats_civils
-  accept_nested_attributes_for :clients_etats_civils
+  accepts_nested_attributes_for :clients_etats_civils
 
 end
